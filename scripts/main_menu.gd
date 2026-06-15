27c8fa31d7a89d938c468003a416694ceb89958b
Pooja Munavath <poojamunavath@gmail.com>
2026-06-15 17:06:39 +0530

    Day 1 completed - menu and scene navigation

---
+++ scripts/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/story_scene.tscn")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
